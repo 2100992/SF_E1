1db7401fd4fd81e6b063107d02861f391f93157d
Shvd <2100992@gmail.com>
2020-01-24 00:30:42 +0300

    experiments

--- bot.py
+++ bot.py
@@ -9,15 +9,17 @@ def start_message(message):
     # print(f'dir(message.chat.id) = {dir(message.chat.id)}')
     print(f'message.chat.id = {message.chat.id}')
 
-    # users[message.chat.id] = User(message.chat.id)
-    # user = users[message.chat.id]
+    if not users.get(message.chat.id):
+        users[message.chat.id] = User(message.chat)
 
-    # print(f'username = {user.username}')
-    # print(f'first_name = {user.first_name}')
-    # print(f'last_name = {user.last_name}')
+    user = users[message.chat.id]
 
-    # bot.send_message(message.chat.id, f'Привет {user.username}, ты написал мне /start')
-    bot.send_message(message.chat.id, f'Привет {message.chat.username}, ты написал мне /start')
+    print(f'username = {user.username}')
+    print(f'first_name = {user.first_name}')
+    print(f'last_name = {user.last_name}')
+
+    bot.send_message(message.chat.id, f'Привет, {user.username}! Ты написал мне /start')
+    # bot.send_message(message.chat.id, f'Привет, {message.chat.username}! Ты написал мне /start')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):

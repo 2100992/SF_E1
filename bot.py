debdd88f05ca813ec8537214c608ac07e0c57d61
Shvd <2100992@gmail.com>
2020-01-23 23:19:47 +0300

    Add bot for telegram

---
+++ bot.py
@@ -0,0 +1,10 @@
+from telegram_bot import TOKEN
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
